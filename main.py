42327f2862e53d28d95e6601188ebe7bc0fff095
erfanRmoghadam <Erfanramezani47@gmail.com>
2026-02-11 04:11:13 -0800

    summary
    -added a nice summary report

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ response = requests.get( URL , params=parameters)
 data = response.json()
 books = data.get("works" , [])
 filtered_books = []
+years = []
 
-
+#loop started
 for book in books :
 
     publish_year = book["first_publish_year"]
@@ -33,11 +34,28 @@ for book in books :
             })
 #loop ended
 
+
 filtered_books.sort(key= lambda x : x["Publish Year"] , reverse=True)
+
 
 with open(output_file , "w" , newline="" , encoding="utf-8") as file:
     writer = csv.DictWriter(file , fieldnames=["Title" , "Author" , "Publish Year" , "Decade" , "Edition Count" ])
     writer.writeheader()
     writer.writerows(filtered_books)
 
-print(f"a total number of {len(filtered_books)} books were written in the CSV file.")
+
+for book in filtered_books:
+    years.append(book["Publish Year"])
+
+newest_book = filtered_books[0]["Title"]
+oldest_book = filtered_books[-1]["Title"]
+
+
+print("\nSummary Report")
+print("-" * 90)
+print(f"Total books fetched : {len(books)}")
+print(f"Books published after {min_year} : {len(filtered_books)}")
+print(f"Newest book was '{newest_book}' which was published in {max(years)}")
+print(f"Oldest book was '{oldest_book}' which was published in {min(years)}")
+print(f"Average of books published years : {sum(years) // len(years)}")
+print(f"Full details of books published after {min_year} were saved in {output_file} file.")
